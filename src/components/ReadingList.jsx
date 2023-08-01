bfbf1d553180fcb07a29194370ec71d8586fd313
MatiSal <salinasmatias100@gmail.com>
2023-08-01 17:13:17 -0300

    add delete botton and check if the book is in array

--- src/components/ReadingList.jsx
+++ src/components/ReadingList.jsx
@@ -1,32 +1,46 @@
 import { useState, useEffect } from 'react';
 import api from '../books.json';
+import { CloseButton } from '@chakra-ui/react'
+
 
 const ReadingList = ({ searchValue, onAddBook }) => {
-    const [searchResult, setSearchResult] = useState([]);
+    const [arrayBooks, setArrayBooks] = useState([]);
 
     useEffect(() => {
         if (searchValue.trim() !== '') {
             const foundBook = api.library.find(
                 (book) => book.book.title.trim().toLowerCase() === searchValue.trim().toLowerCase()
             );
-            setSearchResult([...searchResult, foundBook] || null);
+            if (foundBook && !arrayBooks.some((book) => book.book.title === foundBook.book.title)) {
+                setArrayBooks([...arrayBooks, foundBook]);
+            }
         } else {
-            setSearchResult(null);
+            setArrayBooks([]);
         }
-    }, [searchValue]);
+    }, [searchValue, arrayBooks]);
+
+
+    const deleteBook = (title) => {
+        const updatedArrayBooks = arrayBooks.filter((book) => book.book.title !== title);
+        setArrayBooks(updatedArrayBooks);
+    }
 
     return (
         <div className='containerReading'>
-            {searchResult ? (
+            <h1>Lista de lectura</h1>
+            {arrayBooks ? (
                 <div className='containerList'>
-                {
-                    searchResult.map((b,i)=>(
-                        <div key={i} style={{padding:"5px"}}>
-                            <img src={b.book.cover} alt={b.book.title} width={100} />
-                        </div>
-                    ))
-                }
-                    {/* <img src={searchResult.book.cover} alt={searchResult.book.title} width={200} /> */}
+                    {
+                        arrayBooks.map((b, i) => (
+                            <div key={i} style={{ padding: "10px 5px 5px 5px" }}>
+                                <div className='containerIcon'>
+                                    <CloseButton size='sm' onClick={() => deleteBook(b.book.title)} />
+                                </div>
+
+                                <img src={b.book.cover} alt={b.book.title} width={120} style={{ height: '160px' }} />
+                            </div>
+                        ))
+                    }
                 </div>
             ) : (
                 <p>No se encontró ningún libro con el título {searchValue}</p>
